5f0fc2fdeaef99a615416508fe6db6f512d7134e
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2015-05-20 21:10:48 +0000

    -Muestra en el cerix la deuda asi no este vencida

--- trunk/proteoerp/system/application/controllers/sincro/dispmoviles.php
+++ trunk/proteoerp/system/application/controllers/sincro/dispmoviles.php
@@ -172,7 +172,7 @@ class Dispmoviles extends Controller {
 			if($tabla=='scli'){
 				if($row['vsaldo']>0){
 					$dbscli = $this->db->escape($row['cliente']);
-					$mSQL="SELECT CONCAT(tipo_doc,TRIM(numero)) AS Numero,DATE_FORMAT(fecha, '%Y/%m/%d') AS Fecha, ABS(DATEDIFF(fecha,CURDATE())) AS Dias,monto-abonos AS Saldo FROM smov WHERE tipo_doc NOT IN ('AB','NC','AN') AND monto>abonos AND vence<=CURDATE() AND cod_cli=${dbscli} ORDER BY fecha DESC";
+					$mSQL="SELECT CONCAT(tipo_doc,TRIM(numero)) AS Numero,DATE_FORMAT(fecha, '%Y/%m/%d') AS Fecha, IF(DATEDIFF(CURDATE(),vence)<0,0,DATEDIFF(CURDATE(),vence)) AS Dias,monto-abonos AS Saldo FROM smov WHERE tipo_doc NOT IN ('AB','NC','AN') AND monto>abonos AND cod_cli=${dbscli} ORDER BY fecha DESC";
 					$qq = $this->db->query($mSQL);
 					if($qq->num_rows() > 0){
 						$itdata[]=json_encode($qq->result());
